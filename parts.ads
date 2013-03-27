1937a8a44dd139f2d560fd8d5fed992b6c8e1862
Rasmus Thuresson <rasmusthuresson@msn.com>
2013-03-27 17:23:38 +0100

    Added rotations to parts

--- parts.ads
+++ parts.ads
@@ -19,6 +19,7 @@ package Parts is
 
    type Part_Type(X, Y, Z: Integer) is private;
 
+<<<<<<< HEAD
 --     procedure new_part(Z, Y, X: in Positive; P: out Part_Type);
 --     procedure rotate_X(P: in out Part_Type; Times: Positive);
 --     procedure rotate_Y(P: in out Part_Type; Times: Positive);
@@ -30,6 +31,21 @@ package Parts is
    function Part_To_String(P: in Part_Type) return Unbounded_String;
    function Parse_Part(Str: in Unbounded_String) return Part_Type;
    --function movement_to_string(P: in Part) return String;
+=======
+--     procedure new_part(Z, Y, X : in Positive; P : out Part_Type);
+   procedure Rotate_X(P : in out Part_Type);
+   procedure Rotate_Y(P : in out Part_Type);
+   procedure Rotate_Z(P : in out Part_Type);
+--
+--     procedure move_X(P : in out Part_Type; Steps : Integer);
+--     procedure move_Y(P : in out Part_Type; Steps : Integer);
+--     procedure move_Z(P : in out Part_Type; Steps : Integer);
+
+   procedure Get_Dimensions(P : in Part_Type; X, Y, Z : out Integer);
+   function Part_To_String(P : in Part_Type) return Unbounded_String;
+   function Parse_Part(Str : in Unbounded_String) return Part_Type;
+   --function movement_to_string(P : in Part) return String;
+>>>>>>> Added Rotations for pieces
 --     -- ^nothing to use?
 
 --     procedure Reset(P: in out Part_Type); -Resets to initial position
@@ -41,12 +57,22 @@ private
    type Rot_Arr is
      array(1..3) of Natural;
 
+<<<<<<< HEAD
    type Part_Type(X, Y, Z: Integer) is
       record
          Origin_Displacement: Vec3; -- starts zeroed
          Bounding: AABB; -- cache for Origin_Displacement
          Structure: Structure_Type(X, Y, Z);
          Rotations: Rot_Arr := (others => 0);
+=======
+
+
+   type Part_Type(X, Y, Z : Integer) is
+      record
+         OriginDisplacement : Vec3; -- starts zeroed
+         Structure : Structure_Access := new Structure_Type(X, Y, Z);
+         Rotations : Rot_Arr := (others => 0);
+>>>>>>> Added Rotations for pieces
       end record;
 
 end Parts;
--- structures.ads
+++ structures.ads
@@ -8,26 +8,30 @@
 --
 -----------------------------------------------------------
 with Coordinates; use Coordinates;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Unchecked_Deallocation;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 package Structures is
    type Structure_Type(iX, iY, iZ : Positive) is private;
+   type Structure_Access is access Structure_Type;
 
---     procedure rotate_X(S : in out Structure_Type; Times : Positive);
---     procedure rotate_Y(S : in out Structure_Type; Times : Positive);
---     procedure rotate_Z(S : in out Structure_Type; Times : Positive);
+   procedure Rotate_X(S : in out Structure_Access);
+   procedure Rotate_Y(S : in out Structure_Access);
+   procedure rotate_Z(S : in out Structure_Access);
 --
 --     function collides(a, b : in Structure_Type; Displacement : in Vec3) return boolean;
 --     function get_dimensions(S : in Structure_Type) return Vec3;
 --     -- ^ Needs to return a pair of Vec3! ^
 --     function structure_to_string(S : in Structure_Type) return String;
-   procedure Add(S : in out Structure_Type; X,Y,Z : in Integer);
-   function Is_Occupied(S : in Structure_Type; X,Y,Z : in Integer) return Boolean;
-   procedure Parse_Structure(Str : in Unbounded_String; Struct : in out Structure_Type);
-   function Structure_To_String(Struct : in Structure_Type) return Unbounded_String;
+   procedure Add(S : in out Structure_Access; X,Y,Z : in Integer);
+   function Is_Occupied(S : in Structure_Access; X,Y,Z : in Integer) return Boolean;
+   procedure Parse_Structure(Str : in Unbounded_String; Struct : in out Structure_Access);
+   function Structure_To_String(Struct : in Structure_Access) return Unbounded_String;
 private
    type Test_Arr is
-     array(Integer range <>, Integer range <>, Integer range<>) of Boolean;
+     array(Integer range <>, Integer range <>, Integer range <>) of Boolean;
    type Structure_Type(iX, iY, iZ : Positive) is
       record
          -- ineffective, but it's a start
@@ -36,4 +40,7 @@ private
          Z : Integer := iZ;
          Data : Test_Arr(1..iX, 1..iY, 1..iZ) := (others => (others =>(others => false)));
       end record;
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (Object => Structure_Type, Name => Structure_Access);
 end Structures;
